01e1f4304ee4c8b3baf0abd15ea90bc8e498a59d
ErbolES <rebelmind84@gmail.com>
2025-04-16 21:21:25 +0500

    first commit

---
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.practice
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        val longestWord = findLongestWord(listOf("cat", "dog", "elephant", "abracadabra", null))
+        Log.d("Task 1", longestWord.toString())
+
+        val testWord = "qwe rt"
+        Log.d("Task 2", testWord.formatName().toString())
+
+        val countUniq = countUniqueWords(listOf("hello  world", "hello"))
+        Log.d("Task 3", countUniq.toString())
+
+        Log.d("Task 4", groupUsersByAge(listOf(
+            User("Гоги", 25, "gogi@mail.com"),
+            User("Вася", 18, "vas@mail.com"),
+            User("Qwert", 25, null))).toString())
+
+        Log.d("Task 5",  averageNonNullDescending(listOf(1, 5, null, 10, 3)).toString())
+        Log.d("Task 6",  filterAndFormatEmails(listOf(
+            User("Aswq", 20, "aswq@mail.com"),
+            User("Qwert", 17, "qwert@mail.com"),
+            User("Vgfd", 30, null)
+        )).toString())
+
+        Log.d("Task 7", checkList(listOf(1, 2, 3, 4)).toString())
+        Log.d("Task 7", checkList(listOf(1, -2, 3, 4)).toString())
+
+        Log.d("Task 8", removeDuplicatesAndSort(listOf(5, 1, 2, 5, 3, 1)).toString())
+        Log.d("Task 9", studentsAboveAverage(mapOf(
+            "Аня" to 85,
+            "Олег" to 75,
+            "Ира" to 95
+        )).toString())
+
+
+    }
+}
---
+++ app/src/main/java/com/example/practice/Tasks.kt
@@ -0,0 +1,48 @@
+package com.example.practice
+
+fun findLongestWord(words: List<String?>): String? {
+    return words.filterNotNull().maxByOrNull { it.length }
+}
+
+fun String?.formatName(): String {
+    return this?.trim()?.replace(" ", "")?.lowercase()?.replaceFirstChar { it.uppercase() } ?: ""
+}
+
+fun countUniqueWords(words: List<String>): Int {
+    return words.toSet().size
+}
+
+data class User(val name: String?, val age: Int?, val email: String?)
+fun groupUsersByAge(users: List<User>): Map<Int, Int> {
+    return users.filter { it.age != null }
+        .groupingBy { it.age!! }
+        .eachCount()
+}
+
+fun averageNonNullDescending(numbers: List<Int?>): Double {
+    val filtered = numbers.filterNotNull().sortedDescending()
+    return if (filtered.isNotEmpty()) filtered.average() else 0.0
+}
+
+fun User.isAdult(): Boolean {
+    return this.age != null && this.age >= 18
+}
+fun filterAndFormatEmails(users: List<User>): List<String> {
+    return users.filter { it.isAdult() && it.email != null }
+        .map { it.email!!.uppercase() }
+}
+
+fun checkList(numbers: List<Int>): Pair<Boolean, Boolean> {
+    val allPositive = numbers.all { it > 0 }
+    val anyEven = numbers.any { it % 2 == 0 }
+    return allPositive to anyEven
+}
+
+fun removeDuplicatesAndSort(list: List<Int>): List<Int> {
+    return list.toSet().sorted()
+}
+
+fun studentsAboveAverage(grades: Map<String, Int>): List<String> {
+    val average = grades.values.average()
+    return grades.filter { it.value > average }.keys.toList()
+}
